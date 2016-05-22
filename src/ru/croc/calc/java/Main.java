5633a8db97a6ed1ef62bb95a32afc76d92c0c5af
Andrey Novikov <dromgnh@gmail.com>
2016-05-22 17:20:03 +0300

    Удалены файлы gitignore

---
+++ src/ru/croc/calc/java/Main.java
@@ -0,0 +1,9 @@
+package ru.croc.calc.java;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+        calc.performCalculations(args);
+    }
+}
